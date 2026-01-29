d7397efd2b587455d0903f59627652a49d99b3dc
Mridul Bajpai <mridulbajpai25@gmail.com>
2026-01-30 01:33:50 +0530

    Implement Gram–Schmidt orthogonalization using NumPy

---
+++ LinearAlgebra/gram_schmidt.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def gram_schmidt(basis_vectors):
+  Q=[]
+
+  for v in basis_vectors:
+    u=v.copy()
+
+    for q in Q:
+      u -= np.dot(u,q) * q
+
+
+    q_new = u / np.linalg.norm(u)
+    Q.append(q_new)
+
+    return Q
+  
+
+if __name__ == "__main__":
+  v1 = np.array([1.0, 1.0])
+  v2 = np.array([1.0, 0.0])
+
+  G = gram_schmidt([v1,v2])
+  print(G)
